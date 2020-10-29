21ebf6b2560ffd8e1b3be10fd4cc3242fbbcd95d
Landon LaSmith <LLaSmith@redhat.com>
2020-10-29 16:26:57 -0400

    Add hostIPC enablement to jupyterhub_config

--- jupyterhub/bases/jupyterhub/configs/jupyterhub_config.py
+++ jupyterhub/bases/jupyterhub/configs/jupyterhub_config.py
@@ -1,11 +1,18 @@
 from kubernetes.client import V1Capabilities, V1SELinuxOptions
 spawner = c.OpenShiftSpawner
-def mcs_selinux_profile(spawner, pod):
-  # Apply profile from singleuser-profiles
+def idh_apply_pod_profile(spawner, pod):
+  """Apply custom internal data hub features to pods"""
+
+  # Apply default profile from singleuser-profiles
   apply_pod_profile(spawner, pod)
+
+  # If GPU enabled set the selinux security context
   if spawner.gpu_mode and spawner.gpu_mode == "selinux" and \
        spawner.extra_resource_limits and "nvidia.com/gpu" in spawner.extra_resource_limits:
     # Currently a bug in RHEL Docker 1.13 whereby /dev IPC dirs get inconsistent MCS
     pod.spec.security_context.se_linux_options = V1SELinuxOptions(type='nvidia_container_t',level='s0')
   return pod
-spawner.modify_pod_hook = mcs_selinux_profile
+
+  pod.spec.hostIPC = true
+
+spawner.modify_pod_hook = idh_apply_pod_profile
